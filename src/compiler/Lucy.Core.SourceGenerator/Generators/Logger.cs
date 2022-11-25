58b3cc078d2d5c874c587cb05fe376ecabcaf23d
Daniel Vetter <daniel.vetter86@gmail.com>
2022-11-25 01:06:06 +0100

    Fixed some source generator crashes and added nullability support

--- src/compiler/Lucy.Core.SourceGenerator/Generators/Logger.cs
+++ src/compiler/Lucy.Core.SourceGenerator/Generators/Logger.cs
@@ -1,21 +1,23 @@
-﻿namespace Lucy.Core.SourceGenerator.Generators
+﻿using System.Xml.Linq;
+
+namespace Lucy.Core.SourceGenerator.Generators
 {
     internal class Logger
     {
-        private readonly string _name;
+        private string _name;
         private readonly bool _enabled;
 
         public Logger(string name, bool enabled)
         {
-            _name = name;
-            _enabled = enabled;
-
             if (enabled)
             {
                 if (!Directory.Exists("C:\\generator-log"))
                     Directory.CreateDirectory("C:\\generator-log");
+
+                _name = DateTimeOffset.Now.ToString("yyyy-MM-dd-HH-mm-ss") + " " + name + ".log";
             }
 
+            _enabled = enabled;
         }
 
         public bool IsEnabled => _enabled;
@@ -25,8 +27,13 @@
             if (!_enabled)
                 return;
 
-            message = DateTimeOffset.UtcNow + " " + message;
-            File.AppendAllLines(Path.Combine("C:\\generator-log", _name + ".log"), new[] { message });
+            using var file = File.OpenWrite(Path.Combine("C:\\generator-log", _name));
+            using var sw = new StreamWriter(file);
+
+            file.Seek(0, SeekOrigin.End);
+            sw.WriteLine(DateTimeOffset.UtcNow + " " + message);
+            sw.Flush();
+            file.Flush();
         }
     }
 }
