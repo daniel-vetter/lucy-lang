945a6ebae8135c95cba6492dd5ff333242717d12
Daniel Vetter <daniel.vetter86@gmail.com>
2023-01-04 01:01:25 +0100

    Fixed autocomplete

--- src/compiler/Lucy.Core/SemanticAnalysis/Handler/RangeCalculation.cs
+++ src/compiler/Lucy.Core/SemanticAnalysis/Handler/RangeCalculation.cs
@@ -11,11 +11,27 @@ public static class RangeCalculation
     [GenerateDbExtension] ///<see cref="GetNodeAtPositionEx.GetNodeAtPosition"/>
     public static SyntaxTreeNode? GetNodeAtPosition(IDb db, string documentPath, Position1D position)
     {
+        static SearchResult? Find(IDb db, Position1D position, SyntaxTreeNode startNode, int offset)
+        {
+            if (!startNode.GetChildNodes().Any())
+                return new SearchResult(startNode);
+
+            foreach (var child in startNode.GetChildNodes())
+            {
+                var nodeLength = db.GetNodeRangeLength(child);
+                if (position.Position >= offset && position.Position < +offset + nodeLength.WithTrailingTrivia)
+                    return Find(db, position, child, offset);
+                offset += nodeLength.WithTrailingTrivia;
+            }
+
+            return null;
+        }
+
         var root = db.GetSyntaxTree(documentPath);
-        var result = FindNodeFromPosition(db, position, root, 0);
+        var result = Find(db, position, root, 0);
         return result?.Node;
     }
-
+    
     [GenerateDbExtension] ///<see cref="GetRangeFromNodeEx.GetRangeFromNode"/>
     public static Range1D GetRangeFromNode(IDb db, SyntaxTreeNode node)
     {
@@ -28,26 +44,6 @@ public static class RangeCalculation
     public static Range1D GetRangeFromNodeId(IDb db, INodeId<SyntaxTreeNode> nodeId)
     {
         return db.GetRangeFromNode(db.GetNodeById(nodeId));
-    }
-
-    private static SearchResult? FindNodeFromPosition(IDb db, Position1D position, SyntaxTreeNode startNode, int offset)
-    {
-        if (!startNode.GetChildNodes().Any())
-        {
-            return new SearchResult(startNode);
-        }
-            
-        foreach (var child in startNode.GetChildNodes())
-        {
-            var nodeLength = db.GetNodeRangeLength(child);
-            if (position.Position >= offset && position.Position < + offset + nodeLength.WithoutTrailingTrivia)
-            {
-                return FindNodeFromPosition(db, position, child, offset);
-            }
-            offset += nodeLength.WithTrailingTrivia;
-        }
-
-        return null;
     }
 
     private record SearchResult(SyntaxTreeNode Node);
